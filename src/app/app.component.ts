50b5eee8336368cf31918389a74d228d4de9a386
NapsterAlishaYenery <napsterganc0201@hotmail.com>
2024-09-27 13:34:41 -0400

    Finalizada

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './NavBar/navbar/navbar.component';
+import { HeroComponent } from './HeroSection/hero/hero.component';
+import { SobreMiComponent } from './AboutMe/sobre-mi/sobre-mi.component';
+import { ExperienciaComponent } from './Experience/experiencia/experiencia.component';
+import { ProyectosComponent } from './PtoyectList/proyectos/proyectos.component';
+import { ArticulosComponent } from './Articles/articulos/articulos.component';
+import { ComentariosComponent } from './Coments/comentarios/comentarios.component';
+import { ContactoComponent } from './Contact/contacto/contacto.component';
+import { FooterComponent } from './Footer/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: 
+  [
+    RouterOutlet, 
+    NavbarComponent,
+    HeroComponent,
+    SobreMiComponent,
+    ExperienciaComponent,
+    ProyectosComponent,
+    ArticulosComponent,
+    ComentariosComponent,
+    ContactoComponent,
+    FooterComponent
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
